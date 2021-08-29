84f0a6e0be4abc90ae75610dcd0fd0544dd0b130
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-29 16:07:10 -0500

    few changes

--- Program.cs
+++ Program.cs
@@ -6,14 +6,67 @@ namespace upn_T1_q_7
     {
         static void Main(string[] args)
         {
-            String diaNac, mesNac;
+            String diaNac, mesNac, nroMes;
             Int16 diamesNac;
             Console.WriteLine("Bienvenido a la solución de la pregunta 7:");
             Console.Write("Ingrese día de nacimiento (1,2,3,...,31): ");
             diaNac = Console.ReadLine();
-            Console.Write("Ingrese día mes de nacimiento (1,2,3,...,12): ");
+            Console.Write("Ingrese día mes de nacimiento (enero, febrero, marzo, ..., diciembre): ");
             mesNac = Console.ReadLine();
-            diamesNac = Convert.ToInt16(diaNac + mesNac);
+
+            switch (mesNac)
+            {
+                case "enero":
+                    nroMes = "01";
+                    break;
+                
+                case "febrero":
+                    nroMes = "02";
+                    break;
+
+                case "marzo":
+                    nroMes = "03";
+                    break;
+                
+                case "abril":
+                    nroMes = "04";
+                    break;
+                
+                case "mayo":
+                    nroMes = "05";
+                    break;
+                
+                case "junio":
+                    nroMes = "06";
+                    break;
+                
+                case "julio":
+                    nroMes = "07";
+                    break;
+                
+                case "agosto":
+                    nroMes = "08";
+                    break;
+                
+                case "setiembre":
+                    nroMes = "09";
+                    break;
+                
+                case "octubre":
+                    nroMes = "10";
+                    break;
+                
+                case "noviembre":
+                    nroMes = "11";
+                    break;
+                
+                default:
+                    nroMes = "12";
+                    break;
+                       
+            }
+
+            diamesNac = Convert.ToInt16(diaNac + nroMes);
             Console.WriteLine(Convert.ToString(diamesNac));
 
         }
