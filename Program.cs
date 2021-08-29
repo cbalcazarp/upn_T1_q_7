4b8d0b599005e2ea400f9d04b7d869d034bfcdde
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-29 15:50:58 -0500

    added code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace upn_T1_q_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String diaNac, mesNac;
+            Int16 diamesNac;
+            Console.WriteLine("Bienvenido a la solución de la pregunta 7:");
+            Console.Write("Ingrese día de nacimiento (1,2,3,...,31): ");
+            diaNac = Console.ReadLine();
+            Console.Write("Ingrese día mes de nacimiento (1,2,3,...,12): ");
+            mesNac = Console.ReadLine();
+            diamesNac = Convert.ToInt16(diaNac + mesNac);
+            Console.WriteLine("Bienvenido a la solución de la pregunta 7:");
+
         }
     }
 }
